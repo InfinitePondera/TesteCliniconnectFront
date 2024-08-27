ee0655376484aa6533c633caf2326327d89e2921
InfinitePondera <zyonamadeus01@gmail.com>
2024-08-27 00:33:35 -0300

    Initial commmit

---
+++ src/app/PacienteEdit/PacienteEdit.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'paciente-edit',
+  templateUrl: './pacienteEdit.component.html',
+  styleUrls: ['./pacienteEdit.component.css'],
+  standalone: true
+})
+export class PacienteEdit {
+
+}
